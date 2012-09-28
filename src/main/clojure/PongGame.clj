017452e40a8f5840b5fee9c706717bce6f0d9261
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-27 22:32:46 -0700

    Create a window.

--- src/main/clojure/PongGame.clj
+++ src/main/clojure/PongGame.clj
@@ -1,1 +1,11 @@
+(import '(javax.swing JFrame SwingUtilities WindowConstants))
 
+(defn gameWindow []
+  (doto (JFrame.)
+    (.setSize 800 600)
+    (.setLocationRelativeTo nil)
+    (.setDefaultCloseOperation WindowConstants/EXIT_ON_CLOSE)
+    (.setVisible true)
+    ))
+
+(gameWindow)
