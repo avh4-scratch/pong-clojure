38a8c2f3590ae36f5c3c3f3983bf63defbee81e6
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-27 23:03:47 -0700

    Paint a background color.

--- src/main/clojure/PongGame.clj
+++ src/main/clojure/PongGame.clj
@@ -1,11 +1,26 @@
-(import '(javax.swing JFrame SwingUtilities WindowConstants))
+(import '(javax.swing JFrame JComponent SwingUtilities WindowConstants))
+(import '(java.awt Dimension Graphics Color))
 
-(defn gameWindow []
-  (doto (JFrame.)
-    (.setSize 800 600)
+(defn grassColor []
+  (new Color 87 153 88))
+
+(defn pongCanvas [w h]
+  (proxy [JComponent] []
+    (getPreferredSize [] (new Dimension w h))
+    (paintComponent [g]
+      (doto g
+        (.setColor (grassColor))
+        (.fillRect 0 0 w h)
+        ))
+    ))
+
+(defn pongGame [w h]
+  (doto (new JFrame)
+    (.add (pongCanvas w h))
+    (.pack)
     (.setLocationRelativeTo nil)
     (.setDefaultCloseOperation WindowConstants/EXIT_ON_CLOSE)
     (.setVisible true)
     ))
 
-(gameWindow)
+(SwingUtilities/invokeLater #(pongGame 800 600))
